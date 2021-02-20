c567e73f92e953166895b5ef6e5067d01003210d
vivek <vivek.koyani@gmail.com>
2021-02-21 02:34:08 +0530

    add css and components chats

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import "./App.css";
+import { ChatEngine } from "react-chat-engine";
+import ChatFeed from "./Components/ChatFeed";
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
+    <ChatEngine
+      height="100vh"
+      projectID="e8593e71-ea51-4201-a8db-d5b3ba93c11d"
+      userName="vivek koyani"
+      userSecret="123123"
+      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+    />
   );
 }
 
---
+++ src/Components/ChatFeed.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function ChatFeed() {
+    return (
+        <div>
+            ChatFeed
+        </div>
+    )
+}
